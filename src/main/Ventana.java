78d9d268c16502282b3e906bff93ccaceeb67f85
IsbethC28 <isbethcortez169@gmail.com>
2026-02-20 10:49:52 -0700

    prediseño del login

--- src/main/Ventana.java
+++ src/main/Ventana.java
@@ -2,8 +2,13 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Ventana extends JFrame{
 	
@@ -14,10 +19,49 @@ public class Ventana extends JFrame{
 		this.setSize(500,500);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
-		this.setMinimumSize(new Dimension (200,200));
-		this.setMaximumSize(new Dimension (800,800));
+		this.setMinimumSize(new Dimension (500,500));
+		this.setMaximumSize(new Dimension (600,600));
 		this.setTitle("hola");
 		this.setBackground(Color.BLACK);
+		this.setLayout(null);
+		
+		JPanel contenedor = new JPanel();
+		contenedor.setOpaque(true);
+		contenedor.setBackground(Color.black);
+		contenedor.setSize(500,500);
+		contenedor.setLocation(0, 0);
+		contenedor.setLayout(null);
+		this.add(contenedor);
+		
+		JLabel title_login = new JLabel();
+		title_login.setText("Bienvenido");
+		title_login.setSize(200,30);
+		title_login.setOpaque(true);
+		title_login.setLocation(145, 30);
+		title_login.setBackground(Color.WHITE);
+		title_login.setFont(new Font("Arial",Font.BOLD,22));
+		title_login.setHorizontalAlignment(JLabel.CENTER);
+		contenedor.add(title_login);
+		
+		JTextField username = new JTextField();
+		username.setSize(300,30);
+		username.setLocation(30, 150);
+		username.setFont(new Font("Arial",Font.BOLD,22));
+		contenedor.add(username);
+		
+		JButton acceder = new JButton();
+		acceder.setText("Acceder");
+		acceder.setLocation(175, 400);
+		acceder.setSize(145,50);
+		acceder.setFont(new Font("Arial",Font.BOLD,22));
+		contenedor.add(acceder);
+		
+		
+		contenedor.repaint();
+		contenedor.revalidate();
+		
+		
+		
 	}
 
 }
